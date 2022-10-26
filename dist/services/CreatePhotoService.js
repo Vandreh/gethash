27b257d1c5f76ca2962fa56864560eb04ea1d136
Vandreh <esmeraldovandreh@gmail.com>
2022-10-26 17:03:37 -0300

    fixing package

--- dist/services/CreatePhotoService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreatePhotoService {
-    execute({ title, description, photo }) {
-        console.log(title, description, photo);
-    }
-}
-exports.default = new CreatePhotoService();
